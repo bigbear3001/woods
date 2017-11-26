a8eca45774de2533c09505885b5f04e57c063212
Andreas Perhab <bigbear.ap@gmail.com>
2017-11-26 10:55:08 +0100

    remove unused test

--- src/main/java/com/perhab/games/woods/FulfillmentResponse.java
+++ src/main/java/com/perhab/games/woods/FulfillmentResponse.java
@@ -2,6 +2,9 @@ package com.perhab.games.woods;
 
 import lombok.Data;
 
+/**
+ * @link https://dialogflow.com/docs/fulfillment#response
+ */
 @Data
 public class FulfillmentResponse {
     String speech;
